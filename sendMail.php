c4f586a03d6437516737f2d94b0664d61056cd18
PawelekX <paterpawe@gmail.com>
2022-08-09 14:52:22 +0200

    feat(backend): add php file to send email

--- sendMail.php
+++ sendMail.php
@@ -1,14 +1,33 @@
 <?php
+require_once('class.phpmailer.php');    //dodanie klasy phpmailer
+require_once('class.smtp.php');    //dodanie klasy smtp
+$mail = new PHPMailer();    //utworzenie nowej klasy phpmailer
+
+$from = $_POST['email'];
 $fullName = $_POST['fullName'];
-$email = $_POST['email'];
-$messageContent = $_POST['content'];
+$contentMessage = $_POST['content'];
 
-if (!empty($fullName) || !empty($email) || !empty($messageContent)) {
-    
-    mb_language("uni");
-    mb_internal_encoding("UTF-8");
 
-mb_send_mail()
-}
-
+$mail->From = "j.nowak@webio.pl";    //adres e-mail użyty do wysyłania wiadomości
+$mail->FromName = $fullName;    //imię i nazwisko lub nazwa użyta do wysyłania wiadomości
+$mail->AddReplyTo($email); //adres e-mail nadawcy oraz jego nazwa
+                                             //w polu "Odpowiedz do"  
+$mail->Host = "smtp.webio.pl";    //adres serwera SMTP wysyłającego e-mail
+$mail->Mailer = "smtp";    //do wysłania zostanie użyty serwer SMTP
+$mail->SMTPAuth = true;    //włączenie autoryzacji do serwera SMTP
+$mail->Username = "j.nowak@webio.pl";    //nazwa użytkownika do skrzynki e-mail
+$mail->Password = "hasło";    //hasło użytkownika do skrzynki e-mail
+$mail->Port = 587; //port serwera SMTP zależny od konfiguracji dostawcy usługi poczty
+$mail->Subject = "Formularz kontaktowy";    //Temat wiadomości, można stosować zmienne i znaczniki HTML
+$mail->Body = $contentMessage;    //Treść wiadomości, można stosować zmienne i znaczniki HTML     
+$mail->AddAddress ("biuro@webio.pl","Biuro Webio");    //adres skrzynki e-mail oraz nazwa
+                          //adresata, do którego trafi wiadomość
+ if($mail->Send())    //sprawdzenie wysłania, jeśli wiadomość została pomyślnie wysłana
+    {                      
+    echo 'E-mail został wysłany'; //wyświetl ten komunikat
+    }            
+else    //w przeciwnym wypadku
+    {           
+    echo 'E-mail nie mógł zostać wysłany';    //wyświetl następujący
+    }
 ?>
