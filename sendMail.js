de4c2b8e80d85b2c2d6c8330cd6c06a01f7850fe
PawelekX <paterpawe@gmail.com>
2022-08-10 14:11:52 +0200

    feat(notifications): add notifications send email

--- sendMail.js
+++ sendMail.js
@@ -2,16 +2,30 @@ $document.ready(function() {
     $("#contactForm").on("submit", function(e) {
         e.preventDefault();
         
-        const data = $('#contactForm').serialize();  
-        console.log(data);
+        const data = $('#contactForm').serialize();
         $.ajax({
             url  : "sendMail.php",
             type : "POST",
-            cache:false,
-            data :data,
-            success: function(html) {
-                $('#feedback').html(html);
+            cache: false,
+            data : data,
+            success: function() {
+                const successNotification = document.getElementById('success');
+                successNotification.style.display="flex";
+                
+                setTimeout(() => {
+                    successNotification.style.display="none";
+                  }, 5000);
+                document.getElementById("contactForm").reset();
+            },
+            error: function() {
+               const errorNotification =  document.getElementById('error');
+               errorNotification.style.display="flex";
+               
+                setTimeout(() => {
+                    errorNotification.style.display="none";
+                }, 5000);
             }
-        }); return false;
+        });
+        return false;
     });
 });
